3de764b22d872004cd5007eb4d5738847d98fb6e
noza-lib <souma.nozawa.lib@gmail.com>
2025-11-05 21:46:26 +0900

    1.63

--- js/fadein.js
+++ js/fadein.js
@@ -8,6 +8,20 @@ $(function () {
         $(".header-nav").removeClass("sp_open");
     });
 });
+
+// フェードイン要素をチェックする関数
+function checkFadeInElements() {
+    $('.fadein').each(function () {
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+
+        if (scroll > elemPos - windowHeight + 150) {
+            $(this).addClass('scrollin');
+        }
+    });
+}
+
 $(window).on("load", function () {
     $('.load-fadein').each(function () {
         $(this).addClass('scrollin');
@@ -17,7 +31,19 @@ $(window).on("load", function () {
             $(this).addClass('scrollin');
         });
     }, 1000);
+
+    // ページ読み込み時に既にビューポート内の要素をチェック
+    checkFadeInElements();
 });
+
+// ブラウザの戻るボタンで戻ってきた場合にも実行
+$(window).on("pageshow", function (event) {
+    // ページがキャッシュから復元された場合
+    if (event.originalEvent.persisted) {
+        checkFadeInElements();
+    }
+});
+
 $(window).scroll(function () {
     $('.fadein').each(function () {
         var elemPos = $(this).offset().top,
@@ -29,6 +55,7 @@ $(window).scroll(function () {
         }
     });
 });
+
 $('.accordion-header').click(function () {
     $(this).next().slideToggle();
     $(this).toggleClass('active');
